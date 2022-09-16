013c95950c050a08d34dac60aad8b17dcae64ab3
huang9185 <helyn20012@outlook.com>
2022-09-15 20:49:25 -0400

    Add website folder

--- diet/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Food)
-admin.site.register(Foodgroup)
-admin.site.register(Subgroup)
-admin.site.register(Record)
